c48236b2902bb2b0bf6ea3e44a8beff0186e4c63
Ami Nakano <ami.nakano@outlook.com>
2020-12-28 14:05:04 +0100

    React scaffholding with mui created

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import './App.css';
-import Nav from './Nav';
-import About from './About';
-import Shop from './Shop';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import ItemDetail from './ItemDetail';
-import TopPage from './TopPage';
-import Details from './Details';
-const App = () =>{
+import React, { Component } from 'react';
+import styles from "./App.module.css";
+import { Cards } from "./components";
+import img from "./images/icon.png";
 
-   
-  return (
-    <Router>
-      <div className="App">
-        
-          <Nav />
-          <Switch>
-            <Route path="/" exact component={TopPage}/> 
-            <Route path="/about" component={About}/>
-            <Route path="/shop" exact component={Shop}/>
-            <Route path="/shop/:id" component={ItemDetail}/>
-            <Route path="/recipe/:id" component={Details}/>
-          </Switch>
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { themeFile } from "./util/theme";
 
-        
-      </div>
-    </Router>
-  );
+const theme = createMuiTheme(themeFile);
+
+export class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className={styles.container}>
+          <img src={img} alt="Diamond" />
+          <Cards />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-
-export default App;
+export default App
